ada8e80fc58dd4df57b2a785d710ed6963915c09
Ryan Dymock <rdymock@gmail.com>
2017-08-05 08:42:07 -0400

    add time checking function

--- js/main.js
+++ js/main.js
@@ -1,18 +1,70 @@
+var updateFreq_ms= 1000
+var day = new Date();
+var currMinute = day.getMinutes();
+
+function setCustomMessage(inputMessage){
+    scheduleText.innerHTML = inputMessage 
+}
+
 function updateClock() {
-    var now = new Date(), // current date
-        months = ['January', 'February', '...']; // you get the idea
-        time = now.getHours() + ':' + now.getMinutes(), // again, you get the idea
+    var scheduleText = document.getElementById("scheduleText");
+    scheduleText.innerHTML = "TEST"
 
-        // a cleaner way than string concatenation
-        date = [now.getDate(), 
-                months[now.getMonth()],
-                now.getFullYear()].join(' ');
+    var day = new Date();
+    var today = day.getDay();
+    var hour = day.getHours();
+    var minutes = day.getMinutes();
+    
+//School Day
+if (today > 0 && today < 6) {
+    if (hour == 6) {
+        if (minutes > 30 && minutes < 40) {
+            setCustomMessage("WAKE UP!");
+        } else if (minutes>=40 && minutes < 50){
+            setCustomMessage("Shower");
+        } else if (minutes>=50){
+            setCustomMessage("Get Dressed");
+        }
+    } else if (hour == 7) {
+        if (minutes < 30) {
+            setCustomMessage("Breakfast");
+        } else if (minutes>=30){
+            setCustomMessage("Make Lunch") ;         
+        }
+    }        
+//Saturday
+} else if (today == 6) {
+    if (hour == 8) {
+        if (minutes == 26) {
+            scheduleText.innerHTML = "TEST 26"
+        } else if (minutes == 27) {
+            scheduleText.innerHTML = "TEST 27"
+        } else if (minutes == 28) {
+            scheduleText.innerHTML = "TEST 28"
+        } else if (minutes == 29) {
+            scheduleText.innerHTML = "TEST 29"
+        } else if (minutes == 30) {
+            scheduleText.innerHTML = "TEST 30"
+        }
+    }
+//Church  
+} else if (today == 0) {
+    if (hour == 11) {
+        if (minutes == 00) {
+            scheduleText.innerHTML = "TEST 26"
+        } else if (minutes == 30) {
+            scheduleText.innerHTML = "TEST 27"
+        }
+    }
+}
 
-    // set the content of the element with the ID time to the formatted string
-    document.getElementById('time').innerHTML = [date, time].join(' / ');
-
-    // call this function again in 1000ms
-    setTimeout(updateClock, 1000);
+console.log("checking")
+if(minutes==currMinute+1){
+updateFreq_ms=60000    
+console.log("updated")        
+}
+setTimeout(updateClock, updateFreq_ms);
 }
 
 updateClock();
+console.log("test");
